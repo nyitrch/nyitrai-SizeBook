3fa114c1ee4b0db2c06ba3835516bf4a563f8cb9
Christopher <c.nyitrai@gmail.com>
2017-02-06 04:32:25 -0700

    Added DatePicker to the new record creation screen.

--- app/src/main/java/com/ualberta/nyitrai/nyitrai_sizebook/SizeBookNewRecordActivity.java
+++ app/src/main/java/com/ualberta/nyitrai/nyitrai_sizebook/SizeBookNewRecordActivity.java
@@ -4,9 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -17,11 +17,10 @@ import java.util.Date;
 public class SizeBookNewRecordActivity extends Activity implements SView<SizeBook> {
 
     private EditText recordName;
-    private EditText recordDate;
+    private DatePicker recordDate;
     private EditText recordComment;
 
     private String name;
-    private String tempDate;
     private Date date;
     private String comment;
 
@@ -31,8 +30,11 @@ public class SizeBookNewRecordActivity extends Activity implements SView<SizeBoo
         setContentView(R.layout.newrecord);
 
         recordName = (EditText) findViewById(R.id.recordName);
-        recordDate = (EditText) findViewById(R.id.recordDate);
         recordComment = (EditText) findViewById(R.id.recordComment);
+        recordDate = (DatePicker) findViewById(R.id.recordDate);
+
+        // Set default date for DatePicker to current date.
+        setCurrentDate(recordDate);
 
         Button createButton = (Button) findViewById(R.id.createButton);
 
@@ -41,16 +43,41 @@ public class SizeBookNewRecordActivity extends Activity implements SView<SizeBoo
             public void onClick(View v) {
 
                 name = recordName.getText().toString();
-
-
-                tempDate = recordDate.getText().toString();
-                Calendar cal = Calendar.getInstance();
-                //TODO: Convert the string gotten form the EditText into a Date object.
+                comment = recordComment.getText().toString();
+                date = getDateFromDatePicker(recordDate);
 
                 BookController bc = SizeBookApplication.getBookController();
                 bc.createRecord(name, date, comment);
             }
         });
+    }
+
+    /**
+     * Set default date for DatePicker to current date.
+     * @param datePicker The DatePicker to set the current date to.
+     */
+    public void setCurrentDate(DatePicker datePicker) {
+        Calendar now = Calendar.getInstance();
+
+        int year = now.get(Calendar.YEAR);
+        int month = now.get(Calendar.MONTH);
+        int day = now.get(Calendar.DAY_OF_MONTH);
+
+        datePicker.init(year, month, day, null);
+    }
+
+    /**
+     * Retrieves date from DatePicker object into a Date object.
+     * Method adapted from the following on 2/6/2017.
+     * http://stackoverflow.com/questions/2592499/casting-and-getting-values-from-date-picker-and-time-picker-in-android/14590203#14590203
+     * @param datePicker The DatePicker to retrieve the date from.
+     * @return Date corresponding to the DatePicker.
+     */
+    public Date getDateFromDatePicker(DatePicker datePicker) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(datePicker.getDayOfMonth(), datePicker.getMonth(),
+                datePicker.getYear());
+        return calendar.getTime();
     }
 
     public void update(SizeBook sizeBook) {
